130e9dfce1d4db0a406f75ff4620a8b07ca36e54
Jack Hilscher <jhilscheriv@gmail.com>
2021-07-20 09:35:37 -0500

    Test and code popover on summary form

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Popover from "react-bootstrap/Popover";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 const SummaryForm = () => {
   const [disabled, toggleDisabled] = React.useState(true);
 
+  const popup = (
+    <Popover id="popover-basic">
+      <Popover.Content>No ice cream will actually be delivered</Popover.Content>
+    </Popover>
+  );
+
   const checkboxLabel = (
     <span>
-      I agree to <span style={{ color: "blue" }}>Terms and Conditions</span>
+      I agree to
+      <OverlayTrigger placement="right" overlay={popup}>
+        <span style={{ color: "blue" }}>Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
 
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,11 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved
+} from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
+
 describe("SummaryForm", () => {
   test("Initial conditions", () => {
     render(<SummaryForm />);
@@ -21,10 +27,32 @@ describe("SummaryForm", () => {
 
     expect(button).toBeDisabled();
 
-    fireEvent.click(checkBox);
+    userEvent.click(checkBox);
     expect(button).toBeEnabled();
 
-    fireEvent.click(checkBox);
+    userEvent.click(checkBox);
     expect(button).toBeDisabled();
+  });
+
+  test("popover responds to hover", async () => {
+    render(<SummaryForm />);
+    const nullPopup = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+
+    expect(nullPopup).not.toBeInTheDocument();
+
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+
+    userEvent.hover(termsAndConditions);
+    const popover = screen.getByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popover).toBeInTheDocument();
+
+    userEvent.unhover(termsAndConditions);
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText(/no ice cream will actually be delivered/i)
+    );
   });
 });
